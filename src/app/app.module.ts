a24703969f8ae9aa8bc32b25313b8d54889f0655
anestsiarau <anestsiarau@elinext.com>
2022-11-09 15:42:19 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { Angular4PaystackModule } from 'angular4-paystack';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    Angular4PaystackModule.forRoot('pk_test_c613fc7d428a64fd1e5daea22f8380551b28c78e')
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
